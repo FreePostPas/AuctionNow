e12f56db78d03a7c9f405726167ab409066ce623
FreePostPas <adrien.albaladejo@gmail.com>
2014-12-26 13:55:35 +0100

    Updating auction_model

--- application/libraries/Wow.php
+++ application/libraries/Wow.php
@@ -8,44 +8,43 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	* without any true relation between them
 */
 class Wow {
-        public function money_to_po($original_amount, $formated = FALSE)
-        {
-        	/*
-				* Name: money_to_po()
-				* Transform an amount in the format of WoW : PO/PA/PC
-        	*/
-			$po = floor($original_amount/10000);
-			$pa = floor(($original_amount - $po*10000)/100);
-			$pc = floor($original_amount - $po*10000 - $pa*100);
+	public function money_to_po($original_amount, $formated = FALSE)
+	{
+		/*
+			* Name: money_to_po()
+			* Transform an amount in the format of WoW : PO/PA/PC
+		*/
+		$po = floor($original_amount/10000);
+		$pa = floor(($original_amount - $po*10000)/100);
+		$pc = floor($original_amount - $po*10000 - $pa*100);
 
-			if(!$formated)
-				return array($po, $pa, $pc);
-			else
-				return $po.'PO '.$pa.'PA '.$pc.'PC';
+		if(!$formated)
+			return array($po, $pa, $pc);
+		else
+			return $po.'PO '.$pa.'PA '.$pc.'PC';
+	}
 
-        }
-
-        public function get_item_name_by_id($id)
-        {
-        	/*
-				* Name: get_item_name_by_id($id)
-				* Return the name of item from World's database waiting for a translation by the WowHead
-				* toolkit.
-				* In the case js is disable, this name is the only shown.
-        	*/
+	public function get_item_name_by_id($id)
+	{
+		/*
+			* Name: get_item_name_by_id($id)
+			* Return the name of item from World's database waiting for a translation by the WowHead
+			* toolkit.
+			* In the case js is disable, this name is the only shown.
+		*/
 			
-			$CI =& get_instance();
+		$CI =& get_instance();
 			
-			$worlddb = $CI->load->database('world', TRUE);
-			$query = $worlddb->select('name')->where('entry', $id)->get('item_template'); //Could be entry or id depending of world db version
+		$worlddb = $CI->load->database('world', TRUE);
+		$query = $worlddb->select('name')->where('entry', $id)->get('item_template'); //Could be entry or id depending of world db version
 			
-			if($query->num_rows() > 0)
-			{	
-				$result = $query->row();
-				return $result->name;
-			}
-			return 'Chargement...';
-        }
+		if($query->num_rows() > 0)
+		{	
+			$result = $query->row();
+			return $result->name;
+		}
+		return 'Chargement...';
+	}
 }
 
 /* End of file wow.php */
--- application/models/auction_model.php
+++ application/models/auction_model.php
@@ -21,31 +21,51 @@ class Auction_model extends CI_model
 	public function get_last_ten_auctions()
 	{
 		$this->load->library('Wow'); //To convert standard money to PO/PA/PC and get a name before the wowhead toolkit
+		$this->load->database('character');
 
-		//Waiting for abstraction. SOAP?
-		$data = array(
-					//Format: Auction_guid, item_id, quantity,last_bid_amount, buy_now_amount, seller_name, remaining_time (seconds)
+		$query = $this->db->where('username', strtoupper($username))->get('auctionhouse');
 
-					array('10', '4543', '1', $this->wow->money_to_po('300'), $this->wow->money_to_po('60000'), 'Liuny', '2000'),
-					array('9', '3330', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('600'), 'Liuny', '2000'),
-					array('8', '4542', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('60000'), 'Liuny', '2000'),
-					array('7', '4543', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('60'), 'Gota', '2000'),
-					array('6', '4548', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('602340'), 'Electrizia', '2000'),
-					array('5', '1383', '1', $this->wow->money_to_po('7430000'), $this->wow->money_to_po('400333300'), 'Ures', '2000'),
-					array('4', '5341', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('60000'), 'Gabana', '2000'),
-					array('3', '4234', '20', $this->wow->money_to_po('30000'), $this->wow->money_to_po('60000'), 'SecretMan', '2000'),
-					array('2', '3434', '1', $this->wow->money_to_po('3232400'), $this->wow->money_to_po('60000'), 'AnonymousOuhOuh', '2000'),
-					array('1', '2234', '1', $this->wow->money_to_po('30000'), $this->wow->money_to_po('60000'), 'LoremIpsum', '2000')
-			);
-		return $data;
+		if($query->num_rows() > 0)
+		{
+			$data = array(); //Array of array of unique auction
+			foreach($query->result_array() as $auction)
+			{
+				//Format: Auction_guid, item_id, quantity,last_bid_amount, buy_now_amount, seller_name, remaining_time (seconds)
+				$row = array(
+					'auction_guid'  => $auction['id'],
+					'item_id'  => $auction['itemguid'],
+					'quantity' => $auction['level'],
+					'last_bid_amount'  => $auction['lastbid'],
+					'buy_now_amount' => $auction['buyoutprice'],
+					'seller_name' => $auction['itemowner'],
+					'remaining_time' => time() - $auction['time'] //seconds before auction end
+					);
+				array_push($data, $row);
+			}
+			if(empty($data)) //No need but it is here
+				return NULL;
+			else
+				return $data;
+		}
+		else
+			return NULL;
 	}
 
 	public function get_auction($guid)
 	{
-		//Format: Auction_guid, item_id, quantity,last_bid_amount, buy_now_amount, seller_name, remaining_time (seconds)
-		$data = array('3', '4234', '20', '30000', '60000', 'SecretMan', '2000'); //Waiting for abstraction. SOAP?
+		$this->load->database('character');
 
-		return $data;
+		$query = $this->db->where('username', strtoupper($username))->get('auctionhouse');
+
+		if($query->num_rows() > 0)
+		{
+			$row = $query->row();
+			return array(
+
+				);
+		}
+		else
+			return NULL;
 	}
 
 	public function buy($auction_guid, $character_guid)
